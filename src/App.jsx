98d8da284c22b6bf93fc6660becb3c72bcf24210
rafaeldoloksaribu <bangrafael77@gmail.com>
2026-02-03 16:57:14 +0700

    Fix admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./components/Navbar";
+
+// admin pages
 import Dashboard from "./admin/Dashboard";
 import Booking from "./admin/Booking";
 import Courts from "./admin/Courts";
 import Users from "./admin/Users";
-import Home from "./Home";
-import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Routes>
 
+      {/* USER WEBSITE */}
       <Route
         path="/"
         element={
@@ -20,6 +23,7 @@ export default function App() {
         }
       />
 
+      {/* ADMIN DASHBOARD */}
       <Route path="/admin" element={<Dashboard />} />
       <Route path="/admin/booking" element={<Booking />} />
       <Route path="/admin/courts" element={<Courts />} />
